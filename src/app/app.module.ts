3fd21a1b7a11e3e7b384cc20b91174324dc14ab2
arinakh2005 <arinakholodnytska3@gmail.com>
2024-01-27 14:30:09 +0200

    Feat: Theme preference

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuHeaderComponent } from './components/menu-header/menu-header.component';
+import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { StyleClassModule } from 'primeng/styleclass';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuHeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenubarModule,
+    InputTextModule,
+    PanelMenuModule,
+    StyleClassModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
